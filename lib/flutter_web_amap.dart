08e81449d236c8339fff6229d359d550ed78705e
apollyon <maoexon@gmail.com>
2024-11-08 15:28:54 +0800

    Remove obsolete FlutterWebAmap platform interface and method channel files.

--- lib/flutter_web_amap.dart
+++
@@ -1,8 +0,0 @@
-
-import 'flutter_web_amap_platform_interface.dart';
-
-class FlutterWebAmap {
-  Future<String?> getPlatformVersion() {
-    return FlutterWebAmapPlatform.instance.getPlatformVersion();
-  }
-}
