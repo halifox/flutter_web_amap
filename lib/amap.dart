69b666e90da3f06228acfd0bed2d45e6ba35ef0c
apollyon <maoexon@gmail.com>
2024-11-08 15:30:19 +0800

    Add initial implementation of the amap library with exports for modules.

---
+++ lib/amap.dart
@@ -0,0 +1,22 @@
+/// GetX is an extra-light and powerful multi-platform framework.
+/// It combines high performance state management, intelligent dependency
+/// injection, and route management in a quick and practical way.
+library amap;
+
+export 'js/base/Bounds.dart';
+export 'js/base/LngLat.dart';
+export 'js/base/Pixel.dart';
+export 'js/base/Size.dart';
+export 'js/control/Control.dart';
+export 'js/control/ControlBar.dart';
+export 'js/control/HawkEye.dart';
+export 'js/control/MapType.dart';
+export 'js/control/Scale.dart';
+export 'js/control/ToolBar.dart';
+export 'js/ext.dart';
+export 'js/loader.dart';
+export 'js/location/Geolocation.dart';
+export 'js/search/AutoComplete.dart';
+export 'js/search/CloudDataSearch.dart';
+export 'js/search/PlaceSearch.dart';
+export 'amap_view.dart';
